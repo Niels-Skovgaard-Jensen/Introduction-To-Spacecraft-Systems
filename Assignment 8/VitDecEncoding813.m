e9bc3d7bc8d81de9cc3187cc3c48ec4568c32a91
Niels Jensen <skoogy7@gmail.com>
2020-11-02 23:10:15 +0100

    Progress

--- Assignment 8/VitDecEncoding813.m
+++ Assignment 8/VitDecEncoding813.m
@@ -1,29 +1,36 @@
 clear all; close all;
+%Made by Niels Skovgaard Jensen - November 2020
 
-M = 3
+% From Assignment
+M = 3; %Memory
+trellis=poly2trellis(M+1,[17,15]); %Define Trellis
+%Define Code to decode
+y =  [1,0,1,1,1,0,0,1,0,0,1,0,1,0,1,1,1,1]; %Input
+inf=vitdec(y,trellis,length(y)/2,'term','hard'); %Decode bitstream
+reEncoded = convenc(inf,trellis); %Re-encode for comparison
 
-trellis=poly2trellis(M+1,[17,15]) 
-y =  [1,0,1,1,1,0,0,1,0,0,1,0,1,0,1,1,1,1]
-decode=vitdec(y,trellis,length(y)/2,'term','hard')
-
-
+%Compare input to re-encoded bitstream
 disp('Input');
-disp(y(1:0.5*end));
+disp(y);
 disp('Corrected');
-disp(decode);
-disp('Truth Table 0 means a code correction');
-test = (y(1:0.5*end) ~= decode);
+disp(reEncoded);
+disp('Truth Table 1 means a code correction');
+test = (y ~= reEncoded);
 disp(test);
 disp('Errorcount');
 disp(sum(test));
 
-
-% OUTPUT FOR THIS CASE
+%OUTPUT
 % Input
-%      1     0     1     1     1     0     0     1     0
+%      1     0     1     1     1     0     0     1     0     0     1     0
+%      1     0     1     1     1     1
+% 
 % Corrected
-%      1     0     0     1     1     1     0     0     0
-% Truth Table, 1 means a code correction
-%    0   0   1   0   0   1   0   1   0
+%      1     1     1     1     1     0     0     0     0     0     1     0
+%      1     0     0     1     1     1
+% 
+% Truth Table 1 means a code correction
+%    0   1   0   0   0   0   0   1   0   0   0   0   0   0   1   0   0   0
+% 
 % Errorcount
 %      3
